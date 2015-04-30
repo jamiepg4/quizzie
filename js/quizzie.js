97e2361b6427708a4bcb51a77857437ec4479740
Mike Patek <mpatek@gmail.com>
2015-04-30 15:49:22 -0400

    Actions.

--- js/quizzie.js
+++ js/quizzie.js
@@ -4,14 +4,23 @@ jQuery(document).ready(function($) {
 
 	var QuizzieView = Backbone.View.extend({
 		events: {
-			'click .quizzie-option': 'handleOptionClick'
+			'click .quizzie-option': 'handleOptionClick',
+			'click .quizzie-quiz.answered': 'handleAnsweredClick'
 		},
 		handleOptionClick: function(e) {
 			var optionId = this.$(e.target).closest('.quizzie-option').data('option');
 			this.showOptionChoiceResult(optionId);
 		},
+		handleAnsweredClick: function() {
+			this.$('.quizzie-quiz').removeClass('answered');
+			this.$('.quizzie-result').removeClass('selected');
+		},
 		showOptionChoiceResult: function(optionId) {
-			console.log( 'SHOWING OPTION RESULT: ' + optionId );
+			this.$('.quizzie-quiz').addClass('answered');
+			this.getOptionResultElement(optionId).addClass('selected');
+		},
+		getOptionResultElement: function(optionId) {
+			return this.$('.quizzie-result[data-option="' + optionId + '"]');
 		}
 	});
 
