f10673cd1ce565096353da77baa4de1cdf65b1e2
Mike Patek <mpatek@gmail.com>
2015-04-30 12:11:39 -0400

    Getting templates working.

--- inc/class-quizzie.php
+++ inc/class-quizzie.php
@@ -3,6 +3,7 @@ class Quizzie {
 
 	private $plugin_dir;
 	private $plugin_url;
+	private $shortcode_deps_enqueued = false;
 
 	private static $instance;
 
@@ -21,5 +22,45 @@ class Quizzie {
 	}
 
 	private function setup_actions() {
+		add_shortcode( 'quizzie', array( $this, 'shortcode_handler' ) );
+	}
+
+	public function shortcode_handler( $atts ) {
+		$quiz_id = $this->get_quiz_id_from_shortcode_atts( $atts );
+		if ( ! $quiz_id ) {
+			return;
+		}
+		$this->enqueue_shortcode_deps();
+		return $this->get_quiz_html( $quiz_id );
+	}
+
+	private function get_quiz_id_from_shortcode_atts( $atts ) {
+		$atts = shortcode_atts( array( 'id' => 0 ), $atts );
+		return absint( $atts['id'] );
+	}
+
+	private function get_quiz_html( $quiz_id ) {
+		return '<div class="quizzie" data-id="' . esc_attr( $quiz_id ) . '"></div>';
+	}
+
+	private function enqueue_shortcode_deps() {
+		if ( $this->shortcode_deps_enqueued ) {
+			return;
+		}
+		$this->enqueue_backbone_templates();
+		$this->enqueue_scripts();
+		$this->enqueue_styles();
+		$this->shortcode_deps_enqueued = true;
+	}
+
+	private function enqueue_backbone_templates() {
+		require_once $this->plugin_dir . 'inc/templates/quizzie-backbone.template.php';
+	}
+
+	private function enqueue_scripts() {
+		wp_enqueue_script( 'quizzie', $this->plugin_url . 'js/quizzie.js', array( 'jquery', 'backbone', 'underscore' ), true );
+	}
+
+	private function enqueue_styles() {
 	}
 }
---
+++ inc/templates/quizzie-backbone.template.php
@@ -0,0 +1,5 @@
+ <script type="text/template" id="quizzie-template">
+ 	<div>
+ 		<span class="quizzie-title">The name of our quiz is <%= model.get('name') %></span>
+ 	</div>
+ </script>
